ad76ef1420d00f35f7b103aa750a63c5004aa088
Frost Sigh <frostsigh@gmail.com>
2018-06-20 11:26:37 +0800

    update mysql driver

--- blog_pure_backend/database.py
+++ blog_pure_backend/database.py
@@ -5,9 +5,9 @@ from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine, MetaData
 
-# database_url=mysql+pymysql://user:password@host/blog?charset=utf8mb4
+# database_url=mysql+mysqlconnector://user:password@host/blog
 database_url = os.environ['database_url']
-engine = create_engine(database_url, pool_recycle=3600)
+engine = create_engine(database_url)
 meta = MetaData(engine)
 meta.reflect()
 session_factory = sessionmaker(bind=engine)
